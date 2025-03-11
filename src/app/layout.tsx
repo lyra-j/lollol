a925e22a95e05592e6468b4a48b45b8db4099143
lyra <mk.lyra.j@gmail.com>
2025-03-11 17:50:40 +0900

    🩹 Fix : localFont 수정 및 메타 데이터 수정

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,9 +1,17 @@
 import type { Metadata } from 'next';
+import localFont from 'next/font/local';
 import '../styles/globals.css';
+
+const pretendard = localFont({
+  src: '../public/font/PretendardVariable.woff2',
+  display: 'swap',
+  weight: '100 900',
+  variable: '--font-pretendard'
+});
 
 export const metadata: Metadata = {
   title: 'LoLLoL Next.js App',
-  description: 'Next.js & TS'
+  description: 'Riot Games API를 활용한 LoL 정보 웹 앱입니다.'
 };
 
 export default function RootLayout({
@@ -12,8 +20,8 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
-      <body>{children}</body>
+    <html lang="ko">
+      <body className={`${pretendard.variable} font-pretendard`}>{children}</body>
     </html>
   );
 }
