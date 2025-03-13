db4c25747f0f37f8360e0fd08d70784960ac974e
lyra <mk.lyra.j@gmail.com>
2025-03-13 18:09:33 +0900

    ✨ Update : 챔피언 상세정보를 받아오는 fetch함수 추가 및 img불러올 주소 추가 #4

--- src/constants/constants.ts
+++ src/constants/constants.ts
@@ -1,10 +1,15 @@
 export const LOL_API_URL = 'https://ddragon.leagueoflegends.com';
 export const LOADING_IMG_URL = `${LOL_API_URL}/cdn/img/champion/loading`;
+export const SPLASH_IMG_URL = `${LOL_API_URL}/cdn/img/champion/splash`;
 
 export const DEFAULT_LANGUAGE = 'ko_KR';
 
+// 챔피언 썸네일 이미지 블러오기
+export const championSquareImgUrl = (version: string) => `${LOL_API_URL}/cdn/${version}/img/champion`;
 
+// 챔피언 패시브 스킬 이미지 불러오기
+export const passiveImgUrl = (version: string, passiveImg: string) =>
+  `${LOL_API_URL}/cdn/${version}/img/passive/${passiveImg}`;
 
-
-
-
+// 챔피언 스펠 이미지 불러오기
+export const spellImgUrl = (version: string, spellImg: string) => `${LOL_API_URL}/cdn/${version}/img/spell/${spellImg}`;
--- src/utils/serverApi.ts
+++ src/utils/serverApi.ts
@@ -1,17 +1,25 @@
 import { DEFAULT_LANGUAGE, LOL_API_URL } from '@/constants/constants';
-import { Champion } from '@/types/Champion';
+import { Champion, ChampionDetail } from '@/types/Champion';
 
 // 최신 version 정보 가져오기
 export const getLatestVersion = async (): Promise<string> => {
-  const res = await fetch(`${LOL_API_URL}/api/versions.json`, {
-    method: 'GET',
-  });
+  try {
+    const res = await fetch(`${LOL_API_URL}/api/versions.json`, {
+      method: 'GET',
+    });
 
-  if (!res.ok) {
-    throw new Error(`버전 정보 요청 실패: ${res.status}`);
+    if (!res.ok) {
+      throw new Error(`버전 정보 요청 실패: ${res.status}`);
+    }
+    const data: string[] = await res.json();
+    if (!data.length) {
+      throw new Error(`버전 정보가 없습니다.`);
+    }
+    return data[0]; // 가장 앞에 있는 최신정보 호출
+  } catch (error) {
+    console.error('버전 정보 호출 오류: ', error);
+    throw error;
   }
-  const data: string[] = await res.json();
-  return data[0];
 };
 
 // 챔피언 목록 가져오기 (ISR)
@@ -34,4 +42,20 @@ export const fetchChampionList = async (): Promise<Champion[]> => {
   return Object.values(data.data);
 };
 
+// 챔피언 상세 정보 가져오기 (SSR)
+export const fetchChampionDetail = async (id: string): Promise<ChampionDetail> => {
+  const version = await getLatestVersion();
+  const res = await fetch(`${LOL_API_URL}/cdn/${version}/data/${DEFAULT_LANGUAGE}/champion/${id}.json`, {
+    cache: 'no-cache',
+  });
 
+  // console.log('API 호출 시간:', new Date().toISOString()); // 확인 완료
+
+  if (!res.ok) {
+    throw new Error(`챔피언 상세정보 요청 실패: ${res.status}`);
+  }
+  const data = await res.json();
+  const championDetail = data.data[id];
+
+  return championDetail;
+};
