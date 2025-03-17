4b3fc9e31ce946a9ced4b920a298f623115f477d
lyra <mk.lyra.j@gmail.com>
2025-03-17 09:33:40 +0900

    ✨ Update : 챔피언 로테이션 페이지 구현 #6

--- src/app/rotation/page.tsx
+++ src/app/rotation/page.tsx
@@ -1,5 +1,15 @@
+import RotationChampionList from '@/components/RotationChampionList';
+
 const RotationPage = () => {
-  return <div>RotationPage</div>;
+  return (
+    <section>
+      <article className="flex flex-col p-4">
+        <h2 className="font-pretendard text-4xl font-bold">챔피언 로테이션</h2>
+        <p className="mt-4 text-gray-400">이번주 무료로 플레이 할 수 있어요!</p>
+        <RotationChampionList />
+      </article>
+    </section>
+  );
 };
 
 export default RotationPage;
--- src/components/ChampionCard.tsx
+++ src/components/ChampionCard.tsx
@@ -1,4 +1,4 @@
-import { championSquareImgUrl} from '@/constants/constants';
+import { championSquareImgUrl } from '@/constants/constants';
 import { Champion } from '@/types/Champion';
 import Image from 'next/image';
 import Link from 'next/link';
@@ -11,16 +11,16 @@ const ChampionCard = ({ champion }: ChampionCardProps) => {
   const SQUARE_IMAGE_URL = championSquareImgUrl(champion.version);
 
   return (
-    <li className="p-4 border rounded-lg border-violet-300">
-      <Link href={`/champions/${champion.id}`} className='flex flex-col items-start '>
+    <li className="rounded-lg border border-violet-300 p-4">
+      <Link href={`/champions/${champion.id}`} className="flex flex-col items-start">
         <Image
           src={`${SQUARE_IMAGE_URL}/${champion.id}.png`}
-          alt={`${champion.name} 이미지`}
-          width={200}
-          height={200}
-          className='mx-auto'
+          alt={champion.name}
+          width={150}
+          height={150}
+          className="mx-auto"
         />
-        <h2 className="mt-2 text-xl font-semibold text-violet-500">{champion.name}</h2>
+        <h3 className="mt-2 text-xl font-semibold text-violet-500">{champion.name}</h3>
         <p className="text-gray-400">{champion.title}</p>
       </Link>
     </li>
